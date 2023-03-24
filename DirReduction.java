bc3b2066b34f0a9c53e5a67d5693e95296f48bba
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-24 14:39:58 +0000

    Create DirReduction.java

---
+++ DirReduction.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class DirReduction {
+    public static String[] dirReduc(String[] arr) {
+      if (arr == null || arr.length <= 1) {
+          return arr;
+        }
+      for(int i = 0; i < arr.length-1;i++){
+          if(("NORTH".equals(arr[i])&& "SOUTH".equals(arr[i+1])) ||
+            ("SOUTH".equals(arr[i])&& "NORTH".equals(arr[i+1]))||
+            ("EAST".equals(arr[i])&& "WEST".equals(arr[i+1]))||
+            ("WEST".equals(arr[i])&& "EAST".equals(arr[i+1]))){
+              arr[i] = null;
+              arr[i+1] = null;
+            }
+             
+             
+             
+          
+        }
+        String[] newArr = Arrays.stream(arr)
+                .filter(s -> s != null)
+                .toArray(String[]::new);
+        if(newArr.length == arr.length){
+          return arr;
+        }
+        else{
+          return dirReduc(newArr);
+        }
+    }
+}
