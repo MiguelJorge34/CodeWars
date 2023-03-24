f6e25035fbd5c57dad634387519d30e36600ee32
MiguelJorge34 <55849645+MiguelJorge34@users.noreply.github.com>
2023-03-24 15:17:20 +0000

    Create Scramblies.java

---
+++ Scramblies.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Scramblies {
+    
+    public static boolean scramble(String str1, String str2) {
+      HashMap < Character, Integer> available = new HashMap<>();
+      if(str1 == null || str2 == null){
+        return false;
+      }
+      if(str1 == null || str2 == null){
+        return true;
+      }
+      
+      
+      for(int i = 0; i < str1.length(); i++){
+        if(available.containsKey(str1.charAt(i))){
+          available.put(str1.charAt(i),available.get(str1.charAt(i))+1);
+        }
+        else{
+          available.put(str1.charAt(i),1);
+        }
+          
+      }
+      System.out.println(available);
+      
+      for(int i = 0; i < str2.length() ; i++){
+        if(!available.containsKey(str2.charAt(i)) ){
+          return false;
+        }
+        else if(available.get(str2.charAt(i))==0){
+          return false;
+        }
+        else{
+          available.put(str2.charAt(i),available.get(str2.charAt(i))-1);
+        }
+         
+      }
+      return true;
+    }
+}
